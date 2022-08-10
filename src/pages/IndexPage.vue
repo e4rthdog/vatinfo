d5edf21f02d328ede5014a23fe34fe92ac71f8cd
e4rthdog <elias.stassinos@gmail.com>
2022-08-10 22:43:07 +0300

    WIP

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,18 @@
 <template>
-  <q-page class="flex">
-    <h1>Ton hpiate poikilotropos</h1>
-    <p class="disabled cursor-not-allowed">Ton hpiate poikilotropos</p>
-  </q-page>
+  <div class="q-ma-md justify-evenly">
+    <div class="row justify-evenly">
+      <div class="col-12 col-md-5 bg-orange shadow-5 q-ma-sm">
+        <p>Ton hpiate poikilotropos</p>
+        <div>
+          <q-spinner-pie color="primary" size="2em" />
+          <q-tooltip :offset="[0, 8]">QSpinnerPie</q-tooltip>
+        </div>
+      </div>
+      <div class="col-12 col-md-5 bg-light-green shadow-5 q-ma-sm">
+        <p class="cursor-not-allowed">Ton hpiate poikilotropos</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
