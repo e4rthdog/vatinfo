624bd0693219988830f7c26d04743defa8e5fe29
Elias Stassinos <elias.stassinos@gmail.com>
2022-08-11 12:06:24 +0300

    WIP

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white shadow-5" height-hint="98">
-      <q-toolbar>
+      <q-toolbar class="glossy">
         <q-toolbar-title class="text-weight-thin text-h5 text-center"
           >VATINFO Panels</q-toolbar-title
         >
@@ -12,11 +12,15 @@
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="bg-grey-8 text-white">
-      <div>
-        Made with Quasar {{ $q.version }} - Running on
-        {{ $q.platform.is.name }}, v{{ $q.platform.is.version }}
-      </div>
+    <q-footer elevated class="bg-primary text-white">
+      <q-toolbar class="glossy">
+        <q-toolbar-title class="footer-text text-center">
+          Made with Quasar
+          <q-badge outline color="white">{{ $q.version }}</q-badge> - Running on
+          {{ $q.platform.is.name }},
+          <q-badge outline color="white">v{{ $q.platform.is.version }}</q-badge>
+        </q-toolbar-title>
+      </q-toolbar>
     </q-footer>
   </q-layout>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="q-ma-md justify-evenly">
+  <q-page class="q-ma-md justify-evenly">
     <div class="row justify-evenly">
       <div class="col-12 col-md-5 bg-orange shadow-5 q-ma-sm">
         <p>Ton hpiate poikilotropos</p>
-        <div>
-          <q-spinner-pie color="primary" size="2em" />
-          <q-tooltip :offset="[0, 8]">QSpinnerPie</q-tooltip>
-        </div>
       </div>
       <div class="col-12 col-md-5 bg-light-green shadow-5 q-ma-sm">
         <p class="cursor-not-allowed">Ton hpiate poikilotropos</p>
       </div>
     </div>
-  </div>
+  </q-page>
 </template>
 
 <script setup>
