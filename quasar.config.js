9a99e35792d6701b3d3b3370299dbab5c9ab1b9d
e4rthdog <elias.stassinos@gmail.com>
2022-08-12 08:54:26 +0300

    Convert to scss

--- quasar.config.js
+++ quasar.config.js
@@ -22,7 +22,7 @@ module.exports = configure(function (/* ctx */) {
     boot: ["appGlobals"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: ["app.css"],
+    css: ["app.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
