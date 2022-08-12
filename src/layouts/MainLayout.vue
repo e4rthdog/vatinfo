d3bd95a21f3f37bc8eb9ce58a891de75b13a29a8
e4rthdog <elias.stassinos@gmail.com>
2022-08-12 09:11:22 +0300

    Add open sans as default font

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white shadow-5" height-hint="98">
       <q-toolbar>
-        <q-toolbar-title class="text-weight-thin text-h5 text-center">VATINFO Panels</q-toolbar-title>
+        <q-toolbar-title class="text-weight-400 text-h5 text-center">VATINFO Panels</q-toolbar-title>
         <q-btn @click="$refs.view.updateData()" icon="update" color="positive" label="Refresh All" size="md" />
       </q-toolbar>
     </q-header>
