50f66f38a66d6c36efd0fa692cbcf0297035f8ef
e4rthdog <elias.stassinos@gmail.com>
2022-08-12 14:47:39 +0300

    Finish basic info panel

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -21,6 +21,47 @@
         </q-chip>
       </div>
     </q-card-section>
+    <q-card-section>
+      <div class="row justify-center">
+        <q-tabs no-caps v-model="tabName" dense class="text-black bg-grey-3 full-width" active-color="primary"
+          indicator-color="primary" align="justify" narrow-indicator>
+          <q-tab name="ctr" :label="'Online CTRs - ' + totalCTR.length" />
+          <q-tab name="app" :label="'Online APPs - ' + totalAPP.length" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tabName" animated>
+          <q-tab-panel name="ctr">
+            <div class="row shadow-up-1 q-mt-md" style="font-size:0.8rem;">
+              <div class="col-12" v-for="(m, index) in totalCTR" :key="index">
+                <q-list dense>
+                  <q-item>
+                    <q-item-section>
+                      {{ m.callsign }} since {{ m.time_logon_str }}
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </div>
+            </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="app">
+            <div class="row shadow-up-1 q-mt-md" style="font-size:0.8rem;">
+              <div class="col-12" v-for="(m, index) in totalAPP" :key="index">
+                <q-list dense>
+                  <q-item>
+                    <q-item-section>
+                      {{ m.callsign }} since {{ m.time_logon_str }}
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </div>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </q-card-section>
   </q-card>
 </template>
 
@@ -31,7 +72,20 @@ import { date } from "quasar";
 const cfg = inject("appConfig");
 const allClients = inject('allClients');
 const todaysEvents = ref([]);
+const tabName = ref('ctr');
 
+
+const totalCTR = computed(() => {
+  let res = allClients.value.filter((c) => c.callsign.includes('CTR'))
+  res.forEach((c) => c.time_logon_str = `${c.time_logon.slice(8, 10)}:${c.time_logon.slice(10, 12)}`);
+  return res.sort((a, b) => (a.callsign > b.callsign) ? 1 : -1);
+});
+
+const totalAPP = computed(() => {
+  let res = allClients.value.filter((c) => c.callsign.includes('APP'))
+  res.forEach((c) => c.time_logon_str = `${c.time_logon.slice(8, 10)}:${c.time_logon.slice(10, 12)}`);
+  return res.sort((a, b) => (a.callsign > b.callsign) ? 1 : -1);
+});
 
 const totalByType = computed(() => {
   let res = {}
