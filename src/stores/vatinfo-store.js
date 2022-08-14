b77ceb28d149ffbbdc49f80f468328128301386a
e4rthdog <elias.stassinos@gmail.com>
2022-08-14 17:50:05 +0300

    Fix bug on null localstorage

--- src/stores/vatinfo-store.js
+++ src/stores/vatinfo-store.js
@@ -4,7 +4,7 @@ import { ref } from "vue";
 export const useVatinfoStore = defineStore("vatinfo", () => {
   const saveMetars = (v) => localStorage.setItem("metars", JSON.stringify(v));
   const loadMetars = () => {
-    return JSON.parse(localStorage.getItem("metars"));
+    return JSON.parse(localStorage.getItem("metars")) ?? [];
   };
 
   return { saveMetars, loadMetars };
